b80ec98a3871c4339aa374c17420c67be896b028
RambabuP <rambabu.p@gagriglobal.com>
2025-02-08 12:16:58 +0530

    Initial commit or update

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ShotSpot',
+  webDir: 'www'
+};
+
+export default config;
